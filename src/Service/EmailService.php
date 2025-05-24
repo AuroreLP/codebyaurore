7783cd8b71316c786ab817415e1766f4237a60e4
aurorelp <aleperff@gmail.com>
2025-05-24 14:49:21 +0200

    ajout de validation de l'email expéditeur dans emailservice.php

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -23,6 +23,10 @@ class EmailService
     public function sendContactEmail(string $fromEmail, string $lastname, string $firstname, ?string $phone,
     string $subject, string $message): void
     {
+        if (!filter_var($fromEmail, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException("L'email n'est pas valide.");
+        }
+
         $emailBody = $this->twig->render('emails/contact.html.twig', [
             'lastname' => $lastname,
             'firstname' => $firstname,
